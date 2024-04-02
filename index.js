e7db030b263827a5f5e5356990da2cc385c876ac
mercy-tarus <merciechelah1@gmail.com>
2024-04-02 13:10:42 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,17 @@
+//variable declaration
+let a=2000;
+let b=100;
+let x=2;
+//divide 
+function divide(a,b){
+
+return a/b;
+}
+// square arrow function let x be 2;
+    const square = (x) => {
+        return x * x;
+      }
+    
+//add arrow function add 3 &4
+const add=(parameter1,parameter2) => parameter1+parameter2;
+add(3,4); //=>7
